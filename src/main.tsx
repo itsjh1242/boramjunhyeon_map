6759b0e52e5ee7c5e6827a4245f324e3d72a7327
itsjh <93984898+itsjh1242@users.noreply.github.com>
2025-05-03 18:03:07 +0900

    feat: post card aspect changed

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import AppRoutes from "./Routes";
 import "./style/font.css";
 import "./style/index.css";
 
+const isProd = import.meta.env.MODE === "production";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={isProd ? "/boramjunhyeon_map" : "/"}>
       <AppRoutes />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/boramjunhyeon_map/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
